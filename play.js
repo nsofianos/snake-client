73f16e0692b97b63610fc90cb71f8248d6003080
Nik Sofianos <nsofianos@hotmail.com>
2020-03-11 22:36:42 +0000

    added comments and event handler for user input

--- play.js
+++ play.js
@@ -1,5 +1,26 @@
-const connect = require('./client');
+const connect = require('./client'); //import our connect code
 
 
 console.log('Connecting...');
-connect();
+connect(); // function that connects us to server
+
+const setupInput = () => { 
+  const stdin = process.stdin; //establish standard input
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  
+  const handleUserInput = () => {  //function to handle user inputs
+    stdin.on('data', (key) => { //event handler for user input
+      if (key === '\u0003') { 
+        process.exit(); //exit if user hits ctrl+C
+      }
+    });
+  };
+  handleUserInput(); // initialize user input event handler function
+  return stdin; //return stdin object
+};
+
+setupInput(); //setup user input
+
+
