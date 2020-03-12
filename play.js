50aa698157d0c986ca2dac532c66b3a09d73667c
Nik Sofianos <nsofianos@hotmail.com>
2020-03-12 00:37:47 +0000

    added movement and message keys

--- play.js
+++ play.js
@@ -1,26 +1,7 @@
-const connect = require('./client'); //import our connect code
-
-
+const connect = require('./client'); //import connection module
+const setupInput = require('./input'); //import input module
+const connectObj = connect();
 console.log('Connecting...');
-connect(); // function that connects us to server
-
-const setupInput = () => { 
-  const stdin = process.stdin; //establish standard input
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  
-  const handleUserInput = () => {  //function to handle user inputs
-    stdin.on('data', (key) => { //event handler for user input
-      if (key === '\u0003') { 
-        process.exit(); //exit if user hits ctrl+C
-      }
-    });
-  };
-  handleUserInput(); // initialize user input event handler function
-  return stdin; //return stdin object
-};
-
-setupInput(); //setup user input
+setupInput(connectObj);
 
 
